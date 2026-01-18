c45375b9ae9bffc49f485954492dbd5ee7fc454c
kristmoktan13 <kristmoktan13@gmail.com>
2026-01-18 23:22:26 +0545

    my_sixth_assingment_random_number

---
+++ random_number.py
@@ -0,0 +1,7 @@
+import random
+Code_3_Digit=[random.randint(0,9)for _ in range(3)]
+Code_4_Digit=[random.randint(1,6)for _ in range(4)]
+print("3_Digit combination:",Code_3_Digit)
+print("4_Digit combination:",Code_4_Digit)
+
+
