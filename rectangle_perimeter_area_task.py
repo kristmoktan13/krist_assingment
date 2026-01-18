86ffe2fdc9f5344e1e25aa0e085b2dab9f201edd
kristmoktan13 <kristmoktan13@gmail.com>
2026-01-18 22:04:56 +0545

    my_third_assingment_of_rectangle

---
+++ rectangle_perimeter_area_task.py
@@ -0,0 +1,8 @@
+import math
+rectangle_length= float(input("Input the length of the rectangle: "))
+rectangle_width= float(input("Input the width of the rectangle: "))
+perimeter_of_rectangle=2*(rectangle_length+rectangle_width)
+area_of_rectangle=(rectangle_length * rectangle_width)
+print("The perimeter of rectangle is:"+str(perimeter_of_rectangle))
+print("The area of rectangle is:"+str(area_of_rectangle))
+
