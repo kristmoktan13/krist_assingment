ac557b4f69b563b4de9a7695339d7f059d72d132
kristmoktan13 <kristmoktan13@gmail.com>
2026-03-15 18:04:50 +0200

    Delete Module_3_voting_age.py

--- Module_3_voting_age.py
+++
@@ -1,9 +0,0 @@
-age=int(input("Enter your age:"))
-voting_age=18
-if age>=voting_age:
-    print("You can vote in Finnish Parlimentary Election.")
-else:
-    years_left=voting_age - age
-    print(f"You cannot vote yet.You can vote in {years_left}years.")
-
-
