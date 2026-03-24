83be7e5535196f1dc62b8be1a38b5ae39b53a0f8
kristmoktan13 <kristmoktan13@gmail.com>
2026-03-25 01:10:24 +0200

    Delete Module_6_maximum_number_on_dice.py

--- Module_6_maximum_number_on_dice.py
+++
@@ -1,8 +0,0 @@
-import random
-def roll_dice(sides):
-    return random.randint(1, sides)
-max_number = int(input("Enter the number of sides on the dice: "))
-result = 0
-while result != max_number:
-    result = roll_dice(max_number)
-    print(f"Rolled: {result}")
